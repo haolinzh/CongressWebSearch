705f0071e6d3d1fa4b2a53e814c9ca59b3080a8d
haolinzh <haolinzh@usc.edu>
2016-11-06 00:02:38 -0700

    Big improvement

--- CongressServer.php
+++ CongressServer.php
@@ -7,28 +7,10 @@
  */
 if (isset($_GET["request"]) && ($_GET["request"] == "reqlegislator")):
 
-    if (isset($_GET["legstate"])) {
-        $state = $_GET["legstate"];
-        $reqlegislatorbystate = "http://congress.api.sunlightfoundation.com/legislators?state=" . $state . "&apikey=4acd972a599843bd93ea4dba171a483f&per_page=all";
-        $recvhtml = file_get_contents($reqlegislatorbystate);
-        echo $recvhtml;
-
-    } elseif (isset($_GET["chamber"])) {
+    if (isset($_GET["chamber"])) {
         $chamber = $_GET["chamber"];
         $reqlegislatorbychamer = "http://congress.api.sunlightfoundation.com/legislators?chamber=" . $chamber . "&apikey=4acd972a599843bd93ea4dba171a483f&per_page=all";
         $recvhtml = file_get_contents($reqlegislatorbychamer);
-        echo $recvhtml;
-
-    } elseif (isset($_GET["queryh"])) {
-        $query = $_GET["queryh"];
-        $searchhouse = "http://congress.api.sunlightfoundation.com/legislators?query=" . $query . "&chamber=house&apikey=4acd972a599843bd93ea4dba171a483f&per_page=all";
-        $recvhtml = file_get_contents($searchhouse);
-        echo $recvhtml;
-
-    } elseif (isset($_GET["querys"])) {
-        $query = $_GET["querys"];
-        $searchsenate = "http://congress.api.sunlightfoundation.com/legislators?query=" . $query . "&chamber=senate&apikey=4acd972a599843bd93ea4dba171a483f&per_page=all";
-        $recvhtml = file_get_contents($searchsenate);
         echo $recvhtml;
 
     } else {
@@ -40,14 +22,13 @@ endif;
 
 if (isset($_GET["request"]) && ($_GET["request"] == "reqbills")):
 
-    if (($_GET["active"])=='true') {
+    if (($_GET["active"]) == 'true') {
 
         $reqacbills = "http://congress.api.sunlightfoundation.com/bills?history.active=true&apikey=4acd972a599843bd93ea4dba171a483f&per_page=all";
         $recvhtml = file_get_contents($reqacbills);
         echo $recvhtml;
 
-    }
-    else{
+    } else {
         $reqnwbills = "http://congress.api.sunlightfoundation.com/bills?history.active=false&apikey=4acd972a599843bd93ea4dba171a483f&per_page=all";
         $recvhtml = file_get_contents($reqnwbills);
         echo $recvhtml;
