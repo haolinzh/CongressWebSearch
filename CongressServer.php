9ebf2ba0ae75c5b1fcc2a88f9a01fc40c1da71bb
haolinzh <haolinzh@usc.edu>
2016-11-08 21:34:50 -0800

    Little improvement

--- CongressServer.php
+++ CongressServer.php
@@ -1,4 +1,5 @@
 <?php
+header('Access-Control-Allow-Origin: *');
 /**
  * Created by PhpStorm.
  * User: NOVA
@@ -30,48 +31,46 @@ if (isset($_GET["request"]) && ($_GET["request"] == "reqbills")):
 
     if (($_GET["active"]) == 'true') {
 
-        $reqacbills = "https://congress.api.sunlightfoundation.com/bills?apikey=3e10ee5ae4ca4e5f884cbedf3ef2372a&per_page=50&history.active=true";
+        $reqacbills = "http://congress.api.sunlightfoundation.com/bills?apikey=3e10ee5ae4ca4e5f884cbedf3ef2372a&per_page=50&history.active=true";
         $recvhtml = file_get_contents($reqacbills);
         echo $recvhtml;
 
     }
     else if(($_GET["active"]) == 'false'){
-        $reqnwbills = "https://congress.api.sunlightfoundation.com/bills?apikey=3e10ee5ae4ca4e5f884cbedf3ef2372a&per_page=50&history.active=false";
+        $reqnwbills = "http://congress.api.sunlightfoundation.com/bills?apikey=3e10ee5ae4ca4e5f884cbedf3ef2372a&per_page=50&history.active=false";
         $recvhtml = file_get_contents($reqnwbills);
         echo $recvhtml;
     }
 
-    else if(($_GET["bioid"])){
+    else if(isset($_GET["bioid"])){
         $bioid = $_GET["bioid"];
-        $reqlbillforleg = "https://congress.api.sunlightfoundation.com/bills?sponsor_id=".$bioid."&apikey=3e10ee5ae4ca4e5f884cbedf3ef2372a&per_page=5";
+        $reqlbillforleg = "http://congress.api.sunlightfoundation.com/bills?sponsor_id=".$bioid."&apikey=3e10ee5ae4ca4e5f884cbedf3ef2372a&per_page=5";
         $recvhtml = file_get_contents( $reqlbillforleg);
         echo $recvhtml;
     }
-    else if(($_GET["billid"])){
+    else if(isset($_GET["billid"])){
         $bill = $_GET["billid"];
-        $reqlspecbill = "https://congress.api.sunlightfoundation.com/bills?bill_id=".$bill."&apikey=3e10ee5ae4ca4e5f884cbedf3ef2372a";
+        $reqlspecbill = "http://congress.api.sunlightfoundation.com/bills?bill_id=".$bill."&apikey=3e10ee5ae4ca4e5f884cbedf3ef2372a";
         $recvhtml = file_get_contents( $reqlspecbill);
         echo $recvhtml;
     }
-    else{
-        echo "Wrong";
-    }
+
 
 endif;
 
 
 if (isset($_GET["request"]) && ($_GET["request"] == "reqcommittee")):
 
-    if (($_GET["bioid"])) {
+    if (isset($_GET["bioid"])) {
 
         $bioid=$_GET["bioid"];
-        $reqcomforleg = "https://congress.api.sunlightfoundation.com/committees?member_ids=".$bioid."&apikey=3e10ee5ae4ca4e5f884cbedf3ef2372a&per_page=5";
+        $reqcomforleg = "http://congress.api.sunlightfoundation.com/committees?member_ids=".$bioid."&apikey=3e10ee5ae4ca4e5f884cbedf3ef2372a&per_page=5";
         $recvhtml = file_get_contents($reqcomforleg);
         echo $recvhtml;
 
     } else if(isset($_GET["chamber"])){
         $chamber=$_GET["chamber"];
-        $reqhcom="https://congress.api.sunlightfoundation.com/committees?chamber=".$chamber."&apikey=3e10ee5ae4ca4e5f884cbedf3ef2372a&per_page=all";
+        $reqhcom="http://congress.api.sunlightfoundation.com/committees?chamber=".$chamber."&apikey=3e10ee5ae4ca4e5f884cbedf3ef2372a&per_page=all";
         $recvhtml = file_get_contents($reqhcom);
         echo $recvhtml;
     }
